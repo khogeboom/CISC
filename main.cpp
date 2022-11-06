851addc19f8b9159143d80d672aff75c846289c0
khogeboom <86844592+khogeboom@users.noreply.github.com>
2022-11-06 18:11:52 +0000

    Rotate Doubly Linked List

--- main.cpp
+++ main.cpp
@@ -1,170 +1,273 @@
+//doublyLinked.cpp
+//demonstrates doubly-linked list
+
 #include <iostream>
+
 using namespace std;
 
-int *Merge(int A[], int B[], int sizeA, int sizeB) {
-    int sizeC = sizeA + sizeB;
-    int *C = new int(sizeC);
-    int i = 0;
-    // merge A and B to create C
-    for (int j = 0; j < sizeA; j++) {
-        C[i] = A[j];
-        i++;
-    }
-    for (int k = 0; k < sizeB; k++) {
-        C[i] = B[k];
-        i++;
-    }
+////////////////////////////////////////////////////////////////
 
-    // sort C
-    int temp;
-    int j = 0;
-    for (int i = 0; i < sizeC; i++) {
-        j = i;
-        while (j > 0 && C[j] < C[j-1]) {
-            temp = C[j-1];
-            C[j-1] = C[j];
-            C[j] = temp;
-            j--;
+class Link {
+
+public:
+    double dData; //data item
+
+    Link * pNext; //next link in list
+    Link * pPrevious; //previous link in list
+public:
+    //-------------------------------------------------------------
+    Link(double dd): //constructor
+            dData(dd), pNext(NULL), pPrevious(NULL) {}
+    //-------------------------------------------------------------
+    void displayLink() //display this link
+    {
+        cout << dData << " ";
+    }
+    //-------------------------------------------------------------
+}; //end class Link
+////////////////////////////////////////////////////////////////
+class DoublyLinkedList {
+private:
+    Link * pFirst; //pointer to first item
+    Link * pLast; //pointer to last item
+public:
+    //-------------------------------------------------------------
+    DoublyLinkedList(): //constructor
+            pFirst(NULL), pLast(NULL) {}
+    //-------------------------------------------------------------
+    ~DoublyLinkedList() //destructor (deletes links)
+    {
+        Link * pCurrent = pFirst; //start at beginning of list
+        while (pCurrent != NULL) //until end of list,
+        {
+            Link * pOldCur = pCurrent; //save current link
+            pCurrent = pCurrent -> pNext; //move to next link
+            delete pOldCur; //delete old current
         }
     }
-    return C;
-}
+    //-------------------------------------------------------------
+    bool isEmpty() //true if no links
+    {
+        return pFirst == NULL;
+    }
+    //-------------------------------------------------------------
+    void insertFirst(double dd) //insert at front of list
+    {
+        Link * pNewLink = new Link(dd); //make new link
+        if (isEmpty()) //if empty list,
+            pLast = pNewLink; //newLink <-- last
+        else
+            pFirst -> pPrevious = pNewLink; //newLink <-- old first
+        pNewLink -> pNext = pFirst; //newLink --> old first
+        pFirst = pNewLink; //first --> newLink
+    }
+    //-------------------------------------------------------------
+    void insertLast(double dd) //insert at end of list
+    {
+
+        Link * pNewLink = new Link(dd); //make new link
+
+        if (isEmpty()) //if empty list,
+            pFirst = pNewLink; //first --> newLink
+        else {
+
+            pLast -> pNext = pNewLink; //old last --> newLink
+
+            pNewLink -> pPrevious = pLast; //old last <-- newLink
+
+        }
+        pLast = pNewLink; //newLink <-- last
+
+    }
+    //-------------------------------------------------------------
+    void removeFirst() //remove first link
+    { //(assumes non-empty list)
+
+        Link * pTemp = pFirst;
+
+        if (pFirst -> pNext == NULL) //if only one item
+            pLast = NULL; //null <-- last
+        else
+            pFirst -> pNext -> pPrevious = NULL; //null <-- old next
+        pFirst = pFirst -> pNext; //first --> old next
+
+        delete pTemp; //delete old first
+
+    }
+    //-------------------------------------------------------------
+    void removeLast() //remove last link
+    { //(assumes non-empty list)
+
+        Link * pTemp = pLast;
+
+        if (pFirst -> pNext == NULL) //if only one item
+            pFirst = NULL; //first --> null
+        else
+            pLast -> pPrevious -> pNext = NULL; //old previous --> null
+        pLast = pLast -> pPrevious; //old previous <-- last
+
+        delete pTemp; //delete old last
+
+    }
+    //-------------------------------------------------------------
+    //insert dd just after key
+    bool insertAfter(double key, double dd) { //(assumes non-empty list)
+
+        Link * pCurrent = pFirst; //start at beginning
+
+        while (pCurrent -> dData != key) //until match is found,
+        {
+
+            pCurrent = pCurrent -> pNext; //move to next link
+
+            if (pCurrent == NULL)
+                return false; //didnt find it
+        }
+        Link * pNewLink = new Link(dd); //make new link
+        if (pCurrent == pLast) //if last link,
+        {
+            pNewLink -> pNext = NULL; //newLink --> null
+            pLast = pNewLink; //newLink <-- last
+        } else //not last link,
+        { //newLink --> old next
+            pNewLink -> pNext = pCurrent -> pNext;
+            //newLink <-- old next
+            pCurrent -> pNext -> pPrevious = pNewLink;
+        }
+        pNewLink -> pPrevious = pCurrent; //old current <-- newLink
+        pCurrent -> pNext = pNewLink; //old current --> newLink
+        return true; //found it, did insertion
+    }
+    //-------------------------------------------------------------
+    bool removeKey(double key) //remove item w/ given key
+    { //(assumes non-empty list)
+        Link * pCurrent = pFirst; //start at beginning
+        while (pCurrent -> dData != key) //until match is found,
+        {
+            pCurrent = pCurrent -> pNext; //move to next link
+            if (pCurrent == NULL)
+                return false; //didnt find it
+        }
+        if (pCurrent == pFirst) //found it; first item?
+            pFirst = pCurrent -> pNext; //first --> old next
+        else //not first
+            //old previous --> old next
+            pCurrent -> pPrevious -> pNext = pCurrent -> pNext;
+        if (pCurrent == pLast) //last item?
+            pLast = pCurrent -> pPrevious; //old previous <-- last
+        else //not last
+            //old previous <-- old next
+            pCurrent -> pNext -> pPrevious = pCurrent -> pPrevious;
+        delete pCurrent; //delete item
+        return true; //successful deletion
+    }
+    //-------------------------------------------------------------
+    void displayForward() {
+        cout << "List (first-->last): ";
+        Link * pCurrent = pFirst; //start at beginning
+        while (pCurrent != NULL) //until end of list,
+        {
+            pCurrent -> displayLink(); //display data
+            pCurrent = pCurrent -> pNext; //move to next link
+        }
+        cout << endl;
+    }
+    //-------------------------------------------------------------
+    void displayBackward() {
+
+        cout << "List (last-->first): ";
+
+        Link * pCurrent = pLast; //start at end
+
+        while (pCurrent != NULL) //until start of list,
+        {
+
+            pCurrent -> displayLink(); //display data
+
+            pCurrent = pCurrent -> pPrevious; //go to previous link
+
+        }
+        cout << endl;
+
+    }
+
+    void rotate() {
+        Link* temp = pLast;
+        pLast = pLast -> pPrevious;
+        pLast -> pNext = NULL;
+        temp -> pPrevious = NULL;
+        temp -> pNext = pFirst;
+        pFirst = temp;
+    }
+
+    void reverse() {
+        Link* pCurrent = pFirst;
+        Link* tempPrevious = NULL;
+        while (pCurrent != NULL) {
+            pCurrent -> pPrevious = pCurrent -> pNext;
+            pCurrent -> pNext = tempPrevious;
+            tempPrevious = pCurrent;
+            pCurrent = pCurrent -> pPrevious;
+        }
+        pFirst = tempPrevious;
+    }
+};
 
 int main() {
-    // PART 1 * * * * * * * * * * * *
-    cout << "PART 1" << endl;
-    // get sizes a and b (no larger than 10)
-    int a, b;
-    do {
-        cout << "a = ";
-        cin >> a;
-    } while (a > 10);
-    do {
-        cout << "b = ";
-        cin >> b;
-    } while (b > 10);
 
-    // input random elements into array A and B
-    int A[a];
-    int B[b];
-    for (int i = 0; i < a; i++) {
-        A[i] = (rand() % 100);
-    }
-    for (int j = 0; j < b; j++) {
-        B[j] = (rand() % 100);
-    }
+    DoublyLinkedList theList; //make a new list
+    theList.insertFirst(22); //insert at front
+    theList.insertFirst(44);
 
-    // sort A
-    int temp;
-    int j = 0;
-    for (int i = 0; i < a; i++) {
-        j = i;
-        while (j > 0 && A[j] < A[j-1]) {
-            temp = A[j-1];
-            A[j-1] = A[j];
-            A[j] = temp;
-            j--;
-        }
-    }
-    // sort B
-    j = 0;
-    for (int i = 0; i < b; i++) {
-        j = i;
-        while (j > 0 && B[j] < B[j-1]) {
-            temp = B[j-1];
-            B[j-1] = B[j];
-            B[j] = temp;
-            j--;
-        }
-    }
+    theList.insertFirst(66);
+    theList.insertLast(11); //insert at rear
+    theList.insertLast(33);
 
-    // merge/swap loop
-    int index = 0;
-    while (index < a) {
-        if (B[0] < A[index]) {
-            temp = A[index];
-            A[index] = B[0];
-            B[0] = temp;
-        }
-        // re-sort B
-        int j = 0;
-        for (int i = 0; i < b; i++) {
-            j = i;
-            while (j > 0 && B[j] < B[j-1]) {
-                temp = B[j-1];
-                B[j-1] = B[j];
-                B[j] = temp;
-                j--;
-            }
-        }
-        index++;
-    }
+    theList.insertLast(55);
+    theList.displayForward(); //display list forward
+    theList.displayBackward(); //display list backward
+    cout << "Deleting first, last, and 11" << endl;
+    theList.removeFirst(); //remove first item
 
-    // display arrays
-    cout << "A[] = { ";
-    for (int i = 0; i < a; i++) {
-        if (i != a-1)
-            cout << A[i] << ", ";
-        else
-            cout << A[i] << " }" << endl;
-    }
-    cout << "B[] = { ";
-    for (int j = 0; j < b; j++) {
-        if (j != b-1)
-            cout << B[j] << ", ";
-        else
-            cout << B[j] << " }" << endl;
-    }
+    theList.removeLast(); //remove last item
+
+    theList.removeKey(11); //remove item with key 11
+    theList.displayForward(); //display list forward
+
+    cout << "Inserting 77 after 22, and 88 after 33" << endl;
+    theList.insertAfter(22, 77); //insert 77 after 22
+
+    theList.insertAfter(33, 88); //insert 88 after 33
+    theList.displayForward(); //display list forward
+
+    DoublyLinkedList list1;
+    list1.insertLast(66);
+    list1.insertLast(44);
+    list1.insertLast(22);
+    list1.insertLast(11);
+    list1.insertLast(33);
+    list1.insertLast(55);
+
     cout << endl;
+    list1.displayForward();
 
+    cout << "rotate():" << endl;
+    list1.rotate();
+    list1.displayForward();
+    cout << "rotate()" << endl;
+    list1.rotate();
+    list1.displayForward();
+    cout << "rotate()" << endl;
+    list1.rotate();
+    list1.displayForward();
+    cout << "rotate()" << endl;
+    list1.rotate();
+    list1.displayForward();
 
+    cout << "reverse()" << endl;
+    list1.reverse();
+    list1.displayForward();
 
-    // PART 2 * * * * * * * * * * *
-    cout << "PART 2" << endl;
-    // get size a and b (no larger than 10)
-    do {
-        cout << "a = ";
-        cin >> a;
-    } while (a > 10);
-    do {
-        cout << "b = ";
-        cin >> b;
-    } while (b > 10);
-
-    // input elements into arrays A and B
-    A[a] = {};
-    B[b] = {};
-    for (int i = 0; i < a; i++) {
-        A[i] = (rand() % 100);
-    }
-    for (int j = 0; j < b; j++) {
-        B[j] = (rand() % 100);
-    }
-
-    int *C = Merge(A, B, a, b);
-
-    // display arrays A, B, and C
-    cout << "A[] = { ";
-    for (int i = 0; i < a; i++) {
-        if (i != a-1)
-            cout << A[i] << ", ";
-        else
-            cout << A[i] << " }" << endl;
-    }
-    cout << "B[] = { ";
-    for (int j = 0; j < b; j++) {
-        if (j != b-1)
-            cout << B[j] << ", ";
-        else
-            cout << B[j] << " }" << endl;
-    }
-    cout << "C[] = { ";
-    for (int k = 0; k < (a+b); k++) {
-        if (k != (a+b)-1)
-            cout << C[k] << ", ";
-        else
-            cout << C[k] << " }" << endl;
-    }
 
     return 0;
-}
+} //end main()

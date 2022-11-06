57edf956308de52f5b5262d1c2235bed382dc438
khogeboom <86844592+khogeboom@users.noreply.github.com>
2022-11-06 18:05:06 +0000

    Hash Quadratic

--- main.cpp
+++ main.cpp
@@ -1,51 +1,169 @@
+// Kasey Hogeboom
+
 #include <iostream>
+#include <vector>
+#include <cstdlib>                    //for random numbers
+#include <ctime>                      //for random numbers
 
-#include "Vehicle.h"
-#include "Car.h"
-#include "Truck.h"
-#include "Tanker.h"
 using namespace std;
+////////////////////////////////////////////////////////////////
 
-void Print(Vehicle *);
+class DataItem { //(could have more data)
+
+public:
+    int iData; //data item (key)
+    //--------------------------------------------------------------
+    DataItem(int ii): iData(ii) //constructor
+    {}
+    //--------------------------------------------------------------
+}; //end class DataItem
+////////////////////////////////////////////////////////////////
+
+class HashTable {
+
+private:
+    vector < DataItem * > hashArray; //vector holds hash table
+    int arraySize;
+    DataItem * pNonItem; //for deleted items
+public:
+    //-------------------------------------------------------------
+    HashTable(int size): arraySize(size) //constructor
+    {
+        arraySize = size;
+        hashArray.resize(arraySize); //size the vector
+        for (int j = 0; j < arraySize; j++) //initialize elements
+            hashArray[j] = NULL;
+        pNonItem = new DataItem(-1); //deleted item key is -1
+    }
+    //-------------------------------------------------------------
+    void displayTable()
+    {
+        cout << "Table: ";
+        for (int j = 0; j < arraySize; j++)
+        {
+            if (hashArray[j] != NULL)
+                cout << hashArray[j] -> iData << " ";
+            else
+                cout << "** ";
+        }
+        cout << endl;
+    }
+    //-------------------------------------------------------------
+    int hashFunc(int key)
+    {
+        return key % arraySize; //hash function
+    }
+    //-------------------------------------------------------------
+    void insert(DataItem * pItem) //insert a DataItem
+    //(assumes table not full)
+    {
+        DataItem* key = pItem; //extract key
+        int hashVal = hashFunc(key->iData); //hash the key
+        //until empty cell or -1,
+        int i = 0;
+        int bucketsProbed = 0;
+        while (bucketsProbed < arraySize) {
+            if (hashArray[hashVal] == NULL || hashArray[hashVal] == pNonItem) {
+                hashArray[hashVal] = key;
+                return;
+            }
+
+            i++;
+            hashVal = (hashFunc(key->iData) + i * i) % arraySize;
+            bucketsProbed++;
+        }
+    } //end insert()
+
+    //-------------------------------------------------------------
+    DataItem * remove(int key) //remove a DataItem
+    {
+        int hashVal = hashFunc(key); //hash the key
+        int i = 0;
+        int bucketsProbed = 0;
+        while (hashArray[hashVal] != NULL && bucketsProbed < arraySize) {
+            if (hashArray[hashVal] -> iData == key) {
+                DataItem *temp = hashArray[hashVal];
+                hashArray[hashVal] = pNonItem;
+                return temp;
+            }
+            i++;
+            hashVal = (hashFunc(key) + i * i) % arraySize;
+            bucketsProbed++;
+        }
+        return NULL;
+    } //end remove()
+
+    //-------------------------------------------------------------
+    DataItem * find(int key) //find item with key
+    {
+        int hashVal = hashFunc(key); //hash the key
+        int i = 0;
+        int bucketsProbed = 0;
+        while (hashArray[hashVal] != NULL && bucketsProbed < arraySize) {
+            if (hashArray[hashVal] -> iData == key)
+                return hashArray[hashVal];
+            i++;
+            hashVal = (hashFunc(key) + i * i) % arraySize;
+            bucketsProbed++;
+        }
+        return NULL;
+
+    } //-------------------------------------------------------------
+}; //end class HashTable
+////////////////////////////////////////////////////////////////
 
 int main()
 {
-    const int NUM_VEHICLES = 8;
+    DataItem * pDataItem;
+    int aKey, size;
+    char choice = 'b';
 
-    Car car_1("CAR1_123123123",2);
-    Car car_2("CAR2_1231231234",4);
-
-    Truck truck_1("TRK1_3123123123",2000);
-
-    Truck truck_2("TRK2_4567890123",2100);
-
-    Tanker tanktruck_1("TNKTRK1_3123456",2000);
-
-    Tanker tanktruck_2("TNKTRK2_3456789", 2100);
-
-    truck_1.setTaxRate(0.10,0.15);
-    tanktruck_1.setTaxRate(0.10,0.15);
-
-    // Pointer to array of vehicle objects
-    Vehicle *vehiclePtr[NUM_VEHICLES] =
-            {
-                    &car_1,
-                    &car_2,
-                    &truck_1,
-                    &truck_2,
-                    &tanktruck_1,
-                    &tanktruck_2
-            };
-
-    for (int count = 0; count < NUM_VEHICLES; count++)
-    {
-        Print(vehiclePtr[count]);
+    size = 15;
+    HashTable theHashTable(size);
+    int arr[] = {12, 18, 13, 2, 3, 23, 5, 15, 22};
+    for (int i = 0; i < 9; i++) {
+        pDataItem = new DataItem(arr[i]);
+        theHashTable.insert(pDataItem);
     }
 
+    while (choice != 'x') //interact with user
+    {
+        cout << "Enter first letter of " <<
+             "show, insert, delete, or find: ";
+        cin >> choice;
+        switch (choice)
+        {
+            case 's':
+                theHashTable.displayTable();
+                break;
+            case 'i':
+                cout << "Enter key value to insert: ";
+                cin >> aKey;
+                pDataItem = new DataItem(aKey);
+                theHashTable.insert(pDataItem);
+                theHashTable.displayTable();
+                break;
+            case 'd':
+                cout << "Enter key value to delete: ";
+                cin >> aKey;
+                theHashTable.remove(aKey);
+                theHashTable.displayTable();
+                break;
+            case 'f':
+                cout << "Enter key value to find: ";
+                cin >> aKey;
+                pDataItem = theHashTable.find(aKey);
+                if (pDataItem != NULL)
+                    cout << "Found " << aKey << endl;
+                else
+                    cout << "Could not find " << aKey << endl;
+                theHashTable.displayTable();
+                break;
+            case 'x':
+                break;
+            default:
+                cout << "Invalid entry\n";
+        } //end switch
+    } //end while
     return 0;
-}
-
-void Print(Vehicle * vptr)
-{
-    vptr->print();
-}
+} //end main()
